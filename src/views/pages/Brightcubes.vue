e7658fa7ea3efd212b77ad67e0dd37935949ed05
CodaCrop <wim.d.boer0172@gmail.com>
2018-06-02 18:48:01 +0200

    Update Brightcubes.vue

--- src/views/pages/Brightcubes.vue
+++ src/views/pages/Brightcubes.vue
@@ -1,1 +1,38 @@
+<template lang="jade">
+#work-layer
+  router-link.return-layer-bg(@click.native='$parent.HideHome(false), $parent.SetAnimation(1, 400, 0), $parent.ScrolledPosTop = 0', to='/')
+  .work-bg
+    .project-head-wrapper(:style='WrapperHeadColor')
+    .project-wrapper
+      .project-intro
+        .row
+          .project-intro-title
+            h2.title Ben
+        .row
+          .project-detail-data
+            ul.work-details
+              li
+                p.work-class role :
+                p.work-attribute branding / web design / front end
+              li
+                p.work-class typeface :
+                p.work-attribute Trade Gothic / Proxima Nova
+      .project-items
+        .row(v-for="img in List")
+          .item
+            img(v-lazy="img", :key="img", lazy="loading").web-shot
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+      WrapperHeadColor: 'background-color: #00adef',
+      List: []
+    }
+  }
+}
+</script>
+
+<style>
+</style>
